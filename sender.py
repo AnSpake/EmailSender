460432100c8b29e2a19d5a6db1429f53111c3a7b
Amandine Nassiri <amandine.nassiri@epita.fr>
2020-05-28 01:06:06 +0200

    sender: argparse: directory for emails

--- sender.py
+++ sender.py
@@ -3,6 +3,7 @@
 
 import sys
 import email
+import argparse
 from client_smtp_ssl import EmailClient
 
 # TODO: Launch SMTP server
@@ -11,10 +12,25 @@ SMTP_HOSTNAME = "localhost"
 SMTP_PORT = 465
 
 
+def handle_arg():
+    """
+        Parse CLI arguments
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--directory', required=True,
+                        help="The directory containing the emails to send")
+
+    args = parser.parse_args()
+    return args
+
+
 def main():
     """
         Run SMTP client to send multiple emails stocked in a directory
     """
+
+    args = handle_arg()
+
     with EmailClient(SMTP_HOSTNAME, SMTP_PORT, enable_ssl=False) as server:
         # TODO: iterate for each email in test folder
         with open("test/test-2.eml", 'r') as mail_fd:
