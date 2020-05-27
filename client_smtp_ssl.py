9b0c468aa948d5d263f2db264507e9db8b938af5
Amandine Nassiri <amandine.nassiri@epita.fr>
2020-05-27 18:36:27 +0200

    client: basic lint

--- client_smtp_ssl.py
+++ client_smtp_ssl.py
@@ -1,29 +1,34 @@
 #!/usr/bin/env python3
 
-import smtplib, ssl
+import smtplib
+import ssl
+
 
 def fetch_email_addr(full_msg):
     return full_msg[full_msg.find('<') + 1:full_msg.find('>')]
+
 
 class EmailClient():
     """
         Secure SMTP Client
     """
 
-    def __init__(self, hostname, port, enable_ssl : bool =False):
+    def __init__(self, hostname, port, enable_ssl: bool = False):
         self.hostname = hostname
         self.port = port
 
         if enable_ssl:
             self.context = ssl.create_default_context()
-            self.server = smtplib.SMTP_SSL(hostname, port, context=self.context)
+            self.server = smtplib.SMTP_SSL(hostname,
+                                           port,
+                                           context=self.context)
         else:
             self.server = smtplib.SMTP(hostname, port)
 
     def sendmail(self, email_obj):
-        self.server.sendmail(fetch_email_addr(email_obj['Sender'])
-                            , fetch_email_addr(email_obj['To'])
-                            , email_obj.as_string())
+        self.server.sendmail(fetch_email_addr(email_obj['Sender']),
+                             fetch_email_addr(email_obj['To']),
+                             email_obj.as_string())
 
     def __enter__(self):
         return self
