b86457e6455d59d5346cab8dd2ad7ff496ec6dc8
Amandine Nassiri <amandine.nassiri@epita.fr>
2020-05-27 18:37:00 +0200

    sender: basic lint

--- sender.py
+++ sender.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python3
 
-import smtplib
 import email
-from client_smtp_ssl import EmailServer
+from client_smtp_ssl import EmailClient
 
 # TODO: Launch SMTP server
 # python -m smtpd -n -c DebuggingServer localhost:465
 smtp_hostname = "localhost"
 smtp_port = 465
 
-with EmailServer(smtp_hostname, smtp_port, enable_ssl = False) as server:
+with EmailClient(smtp_hostname, smtp_port, enable_ssl=False) as server:
     # TODO: iterate for each email in test folder
     with open("test/test-2.eml", 'r') as mail_fd:
         mail_obj = email.message_from_file(mail_fd)
