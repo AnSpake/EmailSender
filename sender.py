413cb1d56d75a4fdda43a7195b946ac8ae8a587d
Amandine Nassiri <amandine.nassiri@epita.fr>
2020-05-28 01:40:55 +0200

    sender: handle multiple mails

--- sender.py
+++ sender.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # pylint: disable=missing-module-docstring
 
+import os
 import sys
 import email
 import argparse
@@ -30,11 +31,12 @@ def main():
     args = handle_arg()
 
     with EmailClient(args.hostname, args.port, enable_ssl=False) as server:
-        # TODO: iterate for each email in test folder
-        with open("test/test-2.eml", 'r') as mail_fd:
-            mail_obj = email.message_from_file(mail_fd)
-            # Add log
-            server.sendmail(mail_obj)
+        for subdir, dirs, files in os.walk(args.directory):
+            for filename in files:
+                with open(filename, 'r') as mail_fd:
+                    mail_obj = email.message_from_file(mail_fd)
+                    # Add log
+                    server.sendmail(mail_obj)
 
 if __name__ == "__main__":
     sys.exit(main())
