a87fed4a00ba4b5a33e820558cc627906baf22dc
Amandine Nassiri <amandine.nassiri@epita.fr>
2020-05-27 18:54:52 +0200

    sender: add main

--- sender.py
+++ sender.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
+# pylint: disable=missing-module-docstring
 
+import sys
 import email
 from client_smtp_ssl import EmailClient
 
@@ -8,9 +10,17 @@ from client_smtp_ssl import EmailClient
 SMTP_HOSTNAME = "localhost"
 SMTP_PORT = 465
 
-with EmailClient(SMTP_HOSTNAME, SMTP_PORT, enable_ssl=False) as server:
-    # TODO: iterate for each email in test folder
-    with open("test/test-2.eml", 'r') as mail_fd:
-        mail_obj = email.message_from_file(mail_fd)
-        # Add log
-        server.sendmail(mail_obj)
+
+def main():
+    """
+        Run SMTP client to send multiple emails stocked in a directory
+    """
+    with EmailClient(SMTP_HOSTNAME, SMTP_PORT, enable_ssl=False) as server:
+        # TODO: iterate for each email in test folder
+        with open("test/test-2.eml", 'r') as mail_fd:
+            mail_obj = email.message_from_file(mail_fd)
+            # Add log
+            server.sendmail(mail_obj)
+
+if __name__ == "__main__":
+    sys.exit(main())
